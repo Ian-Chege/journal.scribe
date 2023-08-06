9190ae2a3c115f5e462320cee1c86001b7c2c469
Ian Chege <ianchege001@gmail.com>
2023-08-06 18:31:59 +0300

    feat: creating the search bar powered by AI

--- utils/ai.ts
+++ utils/ai.ts
@@ -1,7 +1,15 @@
 import { OpenAI } from "langchain/llms/openai"
-import { StructuredOutputParser } from "langchain/output_parsers"
-import z from "zod"
 import { PromptTemplate } from "langchain/prompts"
+import { loadQARefineChain } from "langchain/chains"
+import { MemoryVectorStore } from "langchain/vectorstores/memory"
+import { OpenAIEmbeddings } from "langchain/embeddings/openai"
+
+import {
+  StructuredOutputParser,
+  OutputFixingParser,
+} from "langchain/output_parsers"
+import { Document } from "langchain/document"
+import { z } from "zod"
 
 const parser = StructuredOutputParser.fromZodSchema(
   z.object({
@@ -47,4 +55,25 @@ export const analyze = async (content) => {
   } catch (e) {
     console.log(e)
   }
+}
+
+export const qa = async (question, entries) => {
+  const docs = entries.map(
+    (entry) =>
+      new Document({
+        pageContent: entry.content,
+        metadata: { source: entry.id, date: entry.createdAt },
+      })
+  )
+  const model = new OpenAI({ temperature: 0, modelName: "gpt-3.5-turbo" })
+  const chain = loadQARefineChain(model)
+  const embeddings = new OpenAIEmbeddings()
+  const store = await MemoryVectorStore.fromDocuments(docs, embeddings)
+  const relevantDocs = await store.similaritySearch(question)
+  const res = await chain.call({
+    input_documents: relevantDocs,
+    question,
+  })
+
+  return res.output_text
 }
