655409673ca05140502a1cde77af119653309446
Ian Chege <en20101832020@student.mut.ac.ke>
2023-11-28 19:15:12 +0300

    fix: Fix styling issue in title section

--- components/landing-page/title-section.tsx
+++ components/landing-page/title-section.tsx
@@ -13,7 +13,9 @@ const TitleSection: React.FC<TitleSectionProps> = ({
     <>
       <section className="flex flex-col gap-4 justify-center items-start md:items-center">
         <article className="rounded-full p-[1px] text-sm dark:bg-gradient-to-r dark:from-brand-primaryBlue dark:to-brand-primaryPurple">
-          <div className="rounded-full px-3 py-1 dark:bg-black">{pill}</div>
+          <div className="rounded-full w-full px-3 py-1 dark:bg-black">
+            {pill}
+          </div>
         </article>
         {subheading ? (
           <>
